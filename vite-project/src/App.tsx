2fad718fb074a7ac369ea0a6b2de9a8e2f163171
candelabp <109047221+candelabp@users.noreply.github.com>
2024-11-20 22:31:01 -0300

    Initial commit

---
+++ vite-project/src/App.tsx
@@ -0,0 +1,23 @@
+import { ComponentCounter } from "./components/ComponentCounter/ComponentCounter";
+import { ComponentUseEffect } from "./components/ComponentUseEffect/ComponentUseEffect";
+import { FormComponent } from "./components/FormComponent/FormComponent";
+import { MiPrimerComponent } from "./components/Miprimercomponent/MiPrimerComponent"
+
+export const App = ()=>{
+
+    return(
+        
+        <div style={{display:'flex', flexDirection:'column',gap:'2vh'}}>
+            {/* <MiPrimerComponent
+            text={"Texto desde propiedades"}
+            color="red"
+            fontSize={5}
+            />
+            <ComponentCounter />
+            <ComponentUseEffect /> */}
+            {/* <FormComponent /> */}
+
+          
+        </div>
+    );
+};
---
+++ vite-project/src/components/AppProduct/Header/Header.tsx
@@ -0,0 +1,10 @@
+import {Navbar} from 'react-bootstrap';
+export const Header = () => {
+  return (
+    <Navbar className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">Brand Link</Navbar.Brand>
+        </Container>  
+    </Navbar>
+  )
+}
